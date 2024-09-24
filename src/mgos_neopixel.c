33b9d499c21de1faa09cd470be7e5f975fe4e822
Invento-R7 <david.fc@inventoteca.com>
2024-09-24 17:10:33 -0600

    neo bitbang

--- src/mgos_neopixel.c
+++ src/mgos_neopixel.c
@@ -83,15 +83,11 @@ void mgos_neopixel_clear(struct mgos_neopixel *np) {
 }
 
 void mgos_neopixel_show(struct mgos_neopixel *np) {
-  mgos_gpio_write(np->pin, 0);
-  mgos_usleep(100);
-#if MGOS_ENABLE_BITBANG
-  mgos_bitbang_write_bits(np->pin, MGOS_DELAY_100NSEC, 3, 8, 8, 3, np->data,
-                          np->num_pixels * NUM_CHANNELS);
-#endif
-  mgos_gpio_write(np->pin, 0);
-  mgos_usleep(100);
-  mgos_gpio_write(np->pin, 1);
+  uint8_t data = 0;
+  mgos_bitbang_write_bits(np->pin, MGOS_DELAY_USEC, -1, 10, -1, 10, &data, 1);
+  mgos_bitbang_write_bits(np->pin, MGOS_DELAY_100NSEC, 3, 8, 8, 3, np->data, np->num_pixels * NUM_CHANNELS);
+  mgos_bitbang_write_bits(np->pin, MGOS_DELAY_USEC, -1, 10, -1, 10, &data, 1);
+  mgos_bitbang_write_bits(np->pin, MGOS_DELAY_USEC, 10, -1, 10, -1, &data, 1);
 }
 
 void mgos_neopixel_free(struct mgos_neopixel *np) {
